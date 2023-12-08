3abd3dcb16e86be412c02f017f8f8fd34ab49d44
Steven <quezadasteven570@gmail.com>
2023-12-07 19:10:28 -0600

    Login

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos/estilos-index.css">
+  
+</head>
+<body>
+<body>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form>
+        <h3>Login</h3>
+
+        <label for="username">Usuario</label>
+        <input type="text" placeholder="Usuario" id="username">
+
+        <label for="password">Contraseña</label>
+        <input type="password" placeholder="Contraseña" id="password">
+
+        <button>Log In</button>
+        <div class="social">
+
+<p>¡Bienvenido! Estamos aquí para ayudarte a alcanzar tus metas financieras. </p>
+
+        </div>
+    </form>
+</body>
+
+
+
+</body>
+</html>
