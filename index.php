620c415bd91b6e415458c21bb2c16b635d821cd0
Steven <quezadasteven570@gmail.com>
2023-12-07 19:24:57 -0600

    login

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="estilos/estilos-index.css">
+    <title>PrestaMEX</title>
+    <link rel="stylesheet" href="styles/index-styles.css">
     <link rel="stylesheet" type="text/css" href="slide navbar style.css">
+    <link rel="icon" href="images/icono.png" type="image/x-icon">
+
 <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
 </head>
 <body>
