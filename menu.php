4ad799c1bbf878ff8cc0f2c53fc22f3afd74d647
Steven <quezadasteven570@gmail.com>
2023-12-08 19:53:54 -0600

    condicion menu

--- menu.php
+++ menu.php
@@ -15,8 +15,13 @@
           </li>
         </ul>
         <ul class="navbar-nav">
-          <li class="nav-item">
+          <li class="nav-item" id="crearPrestamo" style="display: none;" >
             <a class="nav-link active" aria-current="page" href="lend_index.php">Crear prestamo</a>
+          </li>
+        </ul>
+         <ul class="navbar-nav">
+         <li class="nav-item" id="revisarPrestamo" style="display: none;" >
+            <a class="nav-link active" aria-current="page" href="lend_index.php">Revisar prestamos</a>
           </li>
         </ul>
 
@@ -29,6 +34,20 @@
           window.location.href = 'index.php'; 
         });
       </script>
+      <script>
+    // Realizar una solicitud al servidor para obtener el rol del usuario
+    // Puedes utilizar fetch o XMLHttpRequest para esto
+
+    // Simulando el valor del rol obtenido del servidor (reemplaza esto por tu lógica)
+    const rol = localStorage.getItem('rol');
+
+    if (rol === '1') {
+        // Si el rol es 1, mostramos el enlace para crear préstamo
+        document.getElementById('crearPrestamo').style.display = 'block';
+        document.getElementById('revisarPrestamo').style.display = 'block';
+
+    }
+</script>
 
     </div>
   </nav>
